2e9b4067809aa11e31d13fa70c5d2c5122f380c9
richalex2010 <33013726+richalex2010@users.noreply.github.com>
2018-03-14 03:34:20 -0400

    fetcher.py - added timestamp handling, final text formatting
    Changed output from individual variables to text (just commented out old version). Added code to interpret API's timestamp and handle time zones.

--- NWS fetcher/fetcher.py
+++ NWS fetcher/fetcher.py
@@ -1,26 +1,27 @@
 import requests
 import json
 
-# Access NWS observations for Portland International Jetport and load into variable 'data'
-response = requests.get("https://api.weather.gov/stations/KPWM/observations/current")
+# Important variables
+StationID = "KPWM" #nearest airport code typically
+timeOffset = -4 #timezone (from UTC)
+
+# Access NWS observations and load into variable 'data'
+response = requests.get("https://api.weather.gov/stations/%s/observations/current" % StationID)
 data = response.json()
 
 # Temporary solution to access local JSON file, copied direct from above API
 #filename = 'H:/GitHub/High-Power-Scoreboard/sample.json'
 #if filename:
-#    with open(filename,'r') as f:
+#   with open(filename,'r') as f:
 #        data = json.load(f)
 
-timestamp = data.get('properties').get('timestamp')
+timestampRaw = data.get('properties').get('timestamp')
 temperatureRaw = data.get('properties').get('temperature').get('value')
 dewpointRaw = data.get('properties').get('dewpoint').get('value')
 windDirectionRaw = data.get('properties').get('windDirection').get('value')
 windSpeedRaw = data.get('properties').get('windSpeed').get('value')
 windGustRaw = data.get('properties').get('windGust').get('value')
 barometricPressureRaw = data.get('properties').get('barometricPressure').get('value')
-
-#print(type(timestamp),type(temperatureRaw),type(dewpointRaw),type(windDirectionRaw),type(windSpeedRaw),type(windGustRaw),type(barometricPressureRaw))
-#print(timestamp,temperatureRaw,dewpointRaw,windDirectionRaw,windSpeedRaw,windGustRaw,barometricPressureRaw)
 
 # If data is empty, use 0
 if temperatureRaw is None:
@@ -89,21 +90,105 @@ densityAltitudeM = densityAltitudeFt * 0.3048
 densityAltitudeFt = int(round(densityAltitudeFt))
 densityAltitudeM = int(round(densityAltitudeM))
 
+#parse wind direction
+if (windDirection >= 348.75) and (windDirection <=360) or (windDirection < 11.25) and (windDirection >= 0): 
+    windDirectionStr = "N"
+elif (windDirection >= 11.25) and (windDirection < 33.75): 
+    windDirectionStr = "NNE"
+elif (windDirection >= 33.75) and (windDirection < 56.25):
+    windDirectionStr = "NE"
+elif (windDirection >= 56.25) and (windDirection < 78.75):
+    windDirectionStr = "ENE"
+elif (windDirection >= 78.75) and (windDirection < 101.25):
+    windDirectionStr = "E"
+elif (windDirection >= 101.25) and (windDirection < 123.75):
+    windDirectionStr = "ESE"
+elif (windDirection >= 123.75) and (windDirection < 146.25):
+    windDirectionStr = "SE"
+elif (windDirection >= 146.25) and (windDirection < 168.75):
+    windDirectionStr = "SSE"
+elif (windDirection >= 168.75) and (windDirection < 191.25):
+    windDirectionStr = "S"
+elif (windDirection >= 191.25) and (windDirection < 213.75):
+    windDirectionStr = "SSW"
+elif (windDirection >= 213.75) and (windDirection < 236.25):
+    windDirectionStr = "SW"
+elif (windDirection >= 236.25) and (windDirection < 258.75):
+    windDirectionStr = "WSW"
+elif (windDirection >= 258.75) and (windDirection < 281.25):
+    windDirectionStr = "W"
+elif (windDirection >= 281.25) and (windDirection < 303.75):
+    windDirectionStr = "WNW"
+elif (windDirection >= 303.75) and (windDirection < 326.25):
+    windDirectionStr = "NW"
+elif (windDirection >= 326.25) and (windDirection < 348.75):
+    windDirectionStr = "NNW"
+else:
+    windDirectionStr = "INV"
+
+#work with timestamp, convert to local time
+print(timestampRaw)
+
+timestamp = {
+    'year': timestampRaw[0:4],
+    'month': timestampRaw[5:7],
+    'day': timestampRaw[8:10],
+    'hour': timestampRaw[11:13],
+    'minute': timestampRaw[14:16],
+}
+
+#convert hour and day to int for time zone conversion
+timestamp['hour'] = int(timestamp['hour']) 
+timestamp['day'] = int(timestamp['day'])
+
+#convert time zone
+timestamp['hour'] = timestamp['hour'] + timeOffset 
+
+#if time zone conversion pushes into next/previous day, adjust day and hour accordingly
+if timestamp['hour'] < 0: 
+    timestamp['hour'] = timestamp['hour'] + 24
+    timestamp['day'] = timestamp['day'] - 1
+elif timestamp['hour'] > 24:
+    timestamp['hour'] = timestamp['hour'] - 24
+    timestamp['day'] = timestamp['day'] + 1
+
+#re-add leading zeroes, convert back to string
+if timestamp['hour'] < 10:
+    timestamp['hour'] = str('0%d' % timestamp['hour'])
+else:
+    timestamp['hour'] = str(timestamp['hour'])
+
+if timestamp['day'] < 10:
+    timestamp['day'] = str('0%d' % timestamp['day'])
+else:
+    timestamp['day'] = str(timestamp['day'])
+
+#convert timestamp dictionary to updateTime string
+updateTime = str('Last updated: %s:%s on %s-%s-%s' % (timestamp['hour'],timestamp['minute'],timestamp['month'],timestamp['day'],timestamp['year']))
+
 #display results, US Customary
-print('US Customary:')
-print('Temperature: %d°F' % temperatureF)
-print('Dewpoint: %d°F' % dewpointF)
-print('Wind speed: %d MPH' % windSpeedMPH)
-print('Wind gust: %d MPH' % windGustMPH)
-print('Station pressure: %.2f inHg' % barometricPressureHg)
-print("Density altitude: %d ft" % densityAltitudeFt)
-print('\n')
+#print('US Customary:')
+#print('Temperature: %d°F' % temperatureF)
+#print('Dewpoint: %d°F' % dewpointF)
+#print('Wind speed: %d MPH' % windSpeedMPH)
+#print('Wind gust: %d MPH' % windGustMPH)
+#print('Station pressure: %.2f inHg' % barometricPressureHg)
+#print("Density altitude: %d ft" % densityAltitudeFt)
+#print('\n')
 
 #display results, Metric
-print('Metric:')
-print('Temperature: %d°C' % temperature)
-print('Dewpoint: %d°C' % dewpoint)
-print('Wind speed: %d KPH' % windSpeedKPH)
-print('Wind gust: %d KPH' % windGustKPH)
-print('Station pressure: %.1f hPa' % barometricPressure)
-print("Density altitude: %d m" % densityAltitudeM)
+#print('Metric:')
+#print('Temperature: %d°C' % temperature)
+#print('Dewpoint: %d°C' % dewpoint)
+#print('Wind speed: %d KPH' % windSpeedKPH)
+#print('Wind gust: %d KPH' % windGustKPH)
+#print('Station pressure: %.1f hPa' % barometricPressure)
+#print("Density altitude: %d m" % densityAltitudeM)
+#print('\n')
+
+#simplify to display text
+outputUSC = "%d°F, %s %d G %d mph, %d ft DA" % (temperatureF, windDirectionStr, windSpeedMPH, windGustMPH, densityAltitudeFt)
+outputMetric = "%d°C, %s %d G %d kph, %d m DA" % (temperature, windDirectionStr, windSpeedKPH, windGustKPH, densityAltitudeM)
+print('US Customary:',outputUSC)
+print('Metric:',outputMetric)
+print(updateTime)
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ From PS file:
 	Date/Time in top left - real-time updating
 	Relay and Distance in top center - Updates between relays, manual update necessary? Train someone in pit to make updates
 	Weather in top right - automatically updates from weather station or feed from PWM (calculate DA)
+		Click to open window display time of last update
 	Scarborough Fish & Game in bottom left - link to club website, or rifle page? Maybe competitor/spectator info
 	NRA Mid-Range in bottom center - link to event bulletin (HTML display of same info as on bulletin)
 	All Scores Unofficial in bottom right - link to page explaining that this page is purely for convenience of spectators and competitors, and that no scores are official until published by match organizer.
